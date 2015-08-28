36ad7fbfbee62ff903c1c5e065cff4c3c287c73e
divinity-comp <charlie@divinitycomputing.com>
2015-08-28 21:55:38 +0100

    birthday update

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -48,7 +48,7 @@ function registerGetInfo() {
         profileJSON = result;
        idc("mainDetails").getElementsByTagName("h2")[0].innerHTML = result.first_name;
 		var datesset = result.birthday.split('/');
-       idc("mainDetails").getElementsByTagName("h3")[0].innerHTML = calculateAge(new Date(datesset[0],datesset[1],datesset[2],0,0,0)) + " Years old";
+       idc("mainDetails").getElementsByTagName("h3")[0].innerHTML = calculateAge(new Date(datesset[2],datesset[1],datesset[0],0,0,0)) + " Years old";
 		idc("description").value = result.bio;
     },
     function (error) {
