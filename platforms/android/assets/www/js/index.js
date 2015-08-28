80ac900611eb6c204eaf42797e3ad5d1c3b35c85
divinity-comp <charlie@divinitycomputing.com>
2015-08-28 14:33:32 +0100

    facebook details

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -29,7 +29,7 @@ var app = {
 				registerGetInfo();
 			}
 
-			facebookConnectPlugin.login(["public_profile", "user_birthday","bio","email","first_name","last_name","gender"],
+			facebookConnectPlugin.login(["public_profile", "user_birthday","user_photos","user_hometown","user_likes","user_work_history","user_location","user_about_me","user_actions.books","user_actions.news","user_likes","user_actions.fitness","user_actions.music","user_actions.video"],
 				fbLoginSuccess,
 				function (error) { console.warn("" + error) }
 			);
