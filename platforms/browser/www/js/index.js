5ac4aa0702d2022b20d17031d2ed9d75eb239a92
divinity-comp <charlie@divinitycomputing.com>
2015-09-12 05:19:06 +0100

    aa

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -350,28 +350,29 @@ function register() {
 }
 function searchScreen() {
     newPage("searchscreen.html");
-    alert("searchpage");
-    navigator.geolocation.getCurrentPosition(onSuccessLoc, onErrorLoc);
+    
+    var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+        getUsersBaseOnLocation(position.coords.longitude,position.coords.latitude);  
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 function getUsersBaseOnLocation(longitude,latitude) {
-    ajaxPost(
-        "http://www.divinitycomputing.com/apps/beoples/locationfinder.php", 
-        function (response) {
-        var usersfound = JSON.parse(response);
-        alert(usersfound);
-    },
-    'fbid=' + fbId + '&distance=' + window.localStorage.getItem("distance") + '&longitude=' + longitude + '&latitude=' + latitude);
-}
-
-function onSuccessLoc (position) {
-    getUsersBaseOnLocation(position.coords.longitude,position.coords.latitude);  
-};
-
-// onError Callback receives a PositionError object
-//
-function onErrorLoc(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
 }
 /* Users Details */
 var usersProcessed;
