843b666da4750aa0c09ce1795b86529ba1bed492
divinity-comp <charlie@divinitycomputing.com>
2015-09-19 15:09:22 +0100

    ll

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -436,7 +436,7 @@ function transformUserData() {
                         var tlaa = new TimelineMax();
                             tlaa.set(document.getElementById("viewprofile"), {display:"block"})
                             .fromTo(document.getElementById("viewprofile"), 1, {opacity:"0"}, {opacity:"1",ease: Circ.easeOut},0.5)
-                            .fromTo(document.getElementById("viewprofile").firstChild, 1, {position:"absolute",x:"100%"}, {x:"0%",ease: Circ.easeOut});
+                            .fromTo(document.getElementById("viewprofile").firstChild, 1, {x:"100%"}, {x:"0%",ease: Circ.easeOut});
                         
                         if(dataFromLocation.userprofiles.length != 0) {
                                 ajaxGet(
@@ -481,19 +481,6 @@ function setdataViewprofile(data) {
         viewprofile.getElementsByClassName("mainDetails")[0].children[2].innerHTML = Math.ceil(dataFromLocation.userprofiles[0].distance_in_km) + " km";
     }
 
-    viewprofile.getElementsByClassName("profilemain")[0].getElementsByTagName("p")[0].innerHTML = data.personalData.description;
-    
-    var maininterests = viewprofile.getElementsByClassName("interests")[0];
-    for(i = 0; i < intereststypes.length;i++) {
-        if(personalJSON.interests[intereststypes[i]].length != 0) {
-            maininterests.innerHTML = "<div class='imageint'><img src='img/icons/" + intereststypes[i] +".png'></div>" +maininterests.innerHTML;
-        }
-    }
-    if(maininterests.children == 0) {
-        maininterests.innerHTML = "<h3>No interests</h3>";
-    }
-    viewprofile.getElementsByClassName("whiteback")[0].getElementsByTagName("h2")[1] =  fullJSON.question[data.personalData.question].name;
-    viewprofile.getElementsByClassName("whiteback")[0].getElementsByTagName("h3")[0] =  data.personalData.answer;
     
     dataFromLocation.userprofiles.splice(0, 1);
 }
