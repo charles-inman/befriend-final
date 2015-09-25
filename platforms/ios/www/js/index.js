8f70984700bb5a3c653ad677ba2a3001805f7a46
divinity-comp <charlie@divinitycomputing.com>
2015-09-25 12:03:17 +0100

    sdasd

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -869,22 +869,22 @@ function messageToRecieve() {
 }
 function timeSince(date) {
 
-    var seconds = Math.floor(((new Date().getTime()/1000) - date)),
+    var seconds = Math.floor(((new Date().getTime()/1000) - (date.getTime()/1000) )),
     interval = Math.floor(seconds / 31536000);
 
-    if (interval > 1) return interval + "y";
+    if (interval > 1) return interval + "years";
 
     interval = Math.floor(seconds / 2592000);
-    if (interval > 1) return interval + "m";
+    if (interval > 1) return interval + "minuts";
 
     interval = Math.floor(seconds / 86400);
-    if (interval >= 1) return interval + "d";
+    if (interval >= 1) return interval + "days";
 
     interval = Math.floor(seconds / 3600);
-    if (interval >= 1) return interval + "h";
+    if (interval >= 1) return interval + "hours";
 
     interval = Math.floor(seconds / 60);
-    if (interval > 1) return interval + "m ";
+    if (interval > 1) return interval + "minutes";
 
-    return Math.floor(seconds) + "s";
+    return Math.floor(seconds) + "seconds";
 }
