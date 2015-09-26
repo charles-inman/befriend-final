210852f4a8fc54ceef9f2bf57a73c16ae52c4755
divinity-comp <charlie@divinitycomputing.com>
2015-09-26 09:41:22 +0100

    asdsa

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -905,12 +905,6 @@ function getLastMessages(idcheck) {
         function (response) {
         if(response != "no id") {
             console.log("id check" + response);
-            ajaxPost(
-                "http://www.divinitycomputing.com/apps/beoples/getmessages.php", 
-                function (response) {
-                console.log(response);
-            },
-            'secondaryid=' + idcheck = "&primeid=" + response);
         }
         else {
             alert(response);
