fa934bfe4548f1a68456399564e6b0e761fdb94b
divinity-comp <charlie@divinitycomputing.com>
2015-11-11 13:52:37 +0000

    das

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -559,31 +559,27 @@ function searchProfile() {
     } });
 }
 var userDef = false;
-var usersetAnimation = false;
 var searchAnimation = new TimelineMax();
 function userAnimation() {
-    if(usersetAnimation) {
-        searchAnimation.restart();
-    }
-    else {
+    searchAnimation = new TimelineMax({onComplete:function() {
+         if(userDef == true) {
+            var tlaa = new TimelineMax();
+                tlaa.set(document.getElementById("viewprofile"), {display:"block"})
+                .fromTo(document.getElementById("seachUserLoader"), 1, {x:"0%"}, {x:"-100%",ease: Circ.easeOut},0.5)
+                .fromTo(document.getElementById("viewprofile"), 1, {x:"100%"}, {x:"0%",ease: Circ.easeOut})
+                .fromTo(document.getElementById("viewprofile").firstChild, 1, {y:"100%"}, {y:"0%",ease: Circ.easeOut},"-=0.7")
+                .set(document.getElementById("seachUserLoader"), {display:"none"});
+        }
+        else {
+            TweenLite.fromTo(document.getElementById("seachUserLoader").children, 1, {scale:"1"}, {scale:"0",ease: Back.easeIn.config(1.7), onComplete:function() {
+                searchAnimation.restart();
+            }});
+        }
+    }}
+    );
+    
         searchAnimation.set(document.getElementById("seachUserLoader"), {display:"block",x:0})
-        .staggerFromTo(document.getElementById("seachUserLoader").children, 0.5, {scale:"0"}, {scale:"1",ease: Back.easeOut.config(1.7)},0.3)
-        .set(document.getElementById("seachUserLoader"), {display:"block",onComplete:function() {
-                if(userDef == true) {
-                    var tlaa = new TimelineMax();
-                        tlaa.set(document.getElementById("viewprofile"), {display:"block"})
-                        .fromTo(document.getElementById("seachUserLoader"), 1, {x:"0%"}, {x:"-100%",ease: Circ.easeOut},0.5)
-                        .fromTo(document.getElementById("viewprofile"), 1, {x:"100%"}, {x:"0%",ease: Circ.easeOut})
-                        .fromTo(document.getElementById("viewprofile").firstChild, 1, {y:"100%"}, {y:"0%",ease: Circ.easeOut},"-=0.4")
-                        .set(document.getElementById("seachUserLoader"), {display:"none"});
-                }
-                else {
-                    TweenLite.fromTo(document.getElementById("seachUserLoader").children, 1, {scale:"1"}, {scale:"0",ease: Back.easeIn.config(1.7), onComplete:function() {
-                        searchAnimation.restart();
-                    }});
-                }
-        }});
-    }
+        .staggerFromTo(document.getElementById("seachUserLoader").children, 0.5, {scale:"0"}, {scale:"1",ease: Back.easeOut.config(1.7)},0.3);
 }
 function getUsersBaseOnLocation(longitude,latitude) {
     if(!window.localStorage.getItem("distance")) {
