b8b7707d69cd7bd78f9fbe8743b0977eab97e5db
divinity-comp <charlie@divinitycomputing.com>
2015-12-01 08:36:07 +0000

    fddd

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -249,7 +249,7 @@ function checkFBlogin() {
                 
                 var subscreen = document.createElement("style");
                 subscreen.type = 'text/css';
-                document.body.appendChild(document.createTextNode(".subscreen { height:" + (document.body.clientHeight - (rem * 8)) + "px !important; }"));
+                document.body.appendChild(document.createTextNode(".subscreen { height:" + (document.body.clientHeight - (75)) + "px !important; }"));
             }
 var fullJSON;
 var socket;
